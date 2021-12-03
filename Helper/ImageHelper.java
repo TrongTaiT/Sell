ed4ccab2c4a0905cf2cdf68efb1cbae7162ca2bc
Trong Tai <nguyentrongtai.ait@gmail.com>
2021-12-03 20:19:16 +0700

    xử lý exception bằng throw new RuntimeException(ex);

--- Helper/ImageHelper.java
+++ Helper/ImageHelper.java
@@ -6,6 +6,8 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -23,56 +25,80 @@ import javax.swing.JLabel;
  */
 public class ImageHelper {
 
-    static public ImageIcon resize(JComponent container, String path) throws IOException {
-        BufferedImage image = ImageIO.read(new File(path));
-        return new ImageIcon(image.getScaledInstance(container.getWidth(),
-                container.getHeight(), BufferedImage.SCALE_SMOOTH));
+    static public ImageIcon resize(JComponent container, String path) {
+        try {
+            BufferedImage image = ImageIO.read(new File(path));
+            return new ImageIcon(image.getScaledInstance(container.getWidth(),
+                    container.getHeight(), BufferedImage.SCALE_SMOOTH));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
-    static public byte[] convertToByteArray(String path) throws IOException {
-        BufferedImage bImage = ImageIO.read(new File(path));
-        String formatName = path.substring(path.lastIndexOf('.') + 1);
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ImageIO.write(bImage, formatName, bos);
+    static public byte[] convertToByteArray(String path) {
+        try {
+            BufferedImage bImage = ImageIO.read(new File(path));
+            String formatName = path.substring(path.lastIndexOf('.') + 1);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ImageIO.write(bImage, formatName, bos);
 
-        return bos.toByteArray();
+            return bos.toByteArray();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
-    static public byte[] convertToByteArray(Icon icon) throws IOException {
-        BufferedImage bImage = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(),
-                BufferedImage.SCALE_SMOOTH);
-        Graphics2D g = bImage.createGraphics();
-        g.drawImage(bImage, null, null);
-        icon.paintIcon(null, g, 0, 0);
-        g.dispose();
+    static public byte[] convertToByteArray(Icon icon) {
+        try {
+            BufferedImage bImage = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(),
+                    BufferedImage.SCALE_SMOOTH);
+            Graphics2D g = bImage.createGraphics();
+            g.drawImage(bImage, null, null);
+            icon.paintIcon(null, g, 0, 0);
+            g.dispose();
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(bImage, "jpg", baos);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(bImage, "jpg", baos);
 
-        return baos.toByteArray();
+            return baos.toByteArray();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
-    static public ImageIcon revertFromArrayByte(byte[] imageByte) throws IOException {
-        ByteArrayInputStream bais = new ByteArrayInputStream(imageByte);
-        BufferedImage bImage = ImageIO.read(bais);
+    static public ImageIcon revertFromArrayByte(byte[] imageByte) {
+        try {
+            ByteArrayInputStream bais = new ByteArrayInputStream(imageByte);
+            BufferedImage bImage = ImageIO.read(bais);
 
-        return new ImageIcon(bImage.getScaledInstance(bImage.getWidth(),
-                bImage.getHeight(), BufferedImage.SCALE_SMOOTH));
+            return new ImageIcon(bImage.getScaledInstance(bImage.getWidth(),
+                    bImage.getHeight(), BufferedImage.SCALE_SMOOTH));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
-    static public ImageIcon revertFromArrayByte(JComponent container, byte[] imageByte) throws IOException {
-        ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
-        BufferedImage bImage = ImageIO.read(bis);
+    static public ImageIcon revertFromArrayByte(JComponent container, byte[] imageByte) {
+        try {
+            ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+            BufferedImage bImage = ImageIO.read(bis);
 
-        return new ImageIcon(bImage.getScaledInstance(container.getWidth(),
-                container.getHeight(), BufferedImage.SCALE_SMOOTH));
+            return new ImageIcon(bImage.getScaledInstance(container.getWidth(),
+                    container.getHeight(), BufferedImage.SCALE_SMOOTH));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
-    public static void setDefaultImage(JLabel container) throws IOException {
-        BufferedImage image = ImageIO.read(new File("src\\com\\SELL\\Images", "default_image.png"));
-        // https://stackoverflow.com/questions/9147977/getwidth-and-getheight-are-0-after-calling-setpreferredsize
-        // phải setSize vì component trong panel khi chưa được gọi lên sẽ không có size
-        container.setIcon(new ImageIcon(image.getScaledInstance(190, 240, BufferedImage.SCALE_SMOOTH)));
+    public static void setDefaultImage(JLabel container) {
+        try {
+            BufferedImage image = ImageIO.read(new File("src\\com\\SELL\\Images", "default_image.png"));
+            // https://stackoverflow.com/questions/9147977/getwidth-and-getheight-are-0-after-calling-setpreferredsize
+            // phải setSize vì component trong panel khi chưa được gọi lên sẽ không có size
+            container.setIcon(new ImageIcon(image.getScaledInstance(190, 240, BufferedImage.SCALE_SMOOTH)));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 }
